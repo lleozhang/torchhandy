108cd212a1b1d51e159acf7a6517ac628324c38a
lleozhang <leogamebox@163.com>
2024-02-18 09:49:09 +0800

    fixed embedding

--- torchhandy/Embedding.py
+++ torchhandy/Embedding.py
@@ -18,10 +18,12 @@ class SinPositionalEmbedding(nn.Module):
     
     def forward(self, x):
         x = x.reshape(-1, 1)
-        dtype, device = x.dtype, x.device
+        device = x.device
+        dtype = torch.float32
         half_dim = self.dim // 2
         emb = math.log(10000) / (half_dim - 1)
         emb = torch.exp(torch.arange(half_dim, device = device, dtype = dtype) * -emb)
         emb = x.unsqueeze(1) * emb.unsqueeze(0)
         emb = torch.cat((emb.sin(), emb.cos()), dim = -1).type(dtype)
+        emb = emb.squeeze(1)
         return self.mlp(emb)
